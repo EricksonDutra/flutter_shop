6f8db32ace19bc2f829a48fd188335456d6d8781
EricksonDutra <ericksond10@gmail.com>
2021-10-16 12:20:40 -0400

    storing product in firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const PRODUCT_BASE_URL =
       'https://flutter-shop-2bd71-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL =
+      'https://flutter-shop-2bd71-default-rtdb.firebaseio.com/order';
 }
